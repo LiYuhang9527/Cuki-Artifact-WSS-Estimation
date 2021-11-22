9636fa0390baf260e4aba4ae74728e8408a86811
iluoeli <luoyili233@gmail.com>
2021-11-22 22:04:00 +0800

    plot twitter client

--- src/main/python/plot-twitter-scope.py
+++ src/main/python/plot-twitter-scope.py
@@ -3,53 +3,52 @@ import pandas as pd
 import numpy as np
 import utils
 
-file='G:/datasets/cluster37.0'
-nrows = 1024*1024
+file = 'G:/datasets/cluster37.0'
+nrows = 1024 * 1024
+
 
 def load_csv(filename):
     df = pd.read_csv(filename, header=None, nrows=nrows)
     df = df.reset_index()
     return df.to_numpy()
 
-# 1: item, 4: size
+
+# 2: item, 4: size
+# item foramt: scope1-scope2-scope3-scope4
 X = load_csv(file)
+print(X[0])
 
-num_subplots = 2
+# parse scope
+items = {}
+scopes = [{} for _ in range(0, 5)]
+for item in X[:, 2]:
+    if item not in items:
+        tokens = item.split('-')
+        for i in range(0, len(tokens) - 1):
+            scope = '-'.join(tokens[0:i + 1])
+            if scope not in scopes[i]:
+                scopes[i][scope] = 0
+            scopes[i][scope] += 1
+        items[item] = 0
+for i in range(0, len(scopes)):
+    print('Level:', i, ', num_scopes:', len(scopes[i]))
+
+num_subplots = 5
 fontsize = 12
-plt.figure(0, figsize=(4 * num_subplots, 3))
+plt.figure(0, figsize=(4 * 2, 3 * 4))
+fig, axs = plt.subplots(2, 3)
+fig.tight_layout()
+# plt.subplots_adjust(wspace=0.2, hspace=0.2)
 
-# 1. plot frequency cdf
-ax = plt.subplot(1, num_subplots, 1)
-freq_x, freq_cdf = utils.cdf(X[:, [1]])
-plt.plot(freq_x, freq_cdf, lw=1, linestyle='-', marker='o', markersize=3, markerfacecolor='none',
-         markevery=0.1, label="Twitter")
-plt.xlabel('Item Occurrence', fontsize=fontsize)
-plt.ylabel('CDF', fontsize=fontsize)
-max_freq = max(freq_x)
-print('max freq', max_freq)
-# ax.set_xscale('log', base=10)
-# plt.xlim(left=0)
-# plt.ylim((-2, 2))
-# x_ticks = np.arange(0, max_freq, 5000)
-y_ticks = np.arange(0, 1.2, 0.2)
-# plt.xticks(x_ticks)
-plt.yticks(y_ticks)
-plt.legend(loc='best', ncol=3, fontsize=fontsize)
+# plot per level scope
+for i in range(0, len(scopes)):
+    ax = plt.subplot((num_subplots + 1) / 2, 2, i + 1)
+    scope_cardinality = np.array(list(scopes[i].items()))
+    cardinality = [int(x) for x in scope_cardinality[:, 1]]
+    plt.bar(range(0, len(scope_cardinality[:, 0])), cardinality, label="Twitter")
+    plt.xlabel('Level' + str(i+1), fontsize=fontsize)
+    plt.ylabel('Cardinality', fontsize=fontsize)
+    plt.legend(loc='best', ncol=3, fontsize=fontsize)
 
-# 2. plot size cdf
-ax = plt.subplot(1, num_subplots, 2)
-size_x, size_cdf = utils.cdf2(X[:, [4]])
-plt.plot(size_x, size_cdf, lw=1, linestyle='-', marker='o', markersize=3, markerfacecolor='none',
-         markevery=0.1, label="Twitter")
-plt.xlabel('Request Size (Byte)', fontsize=fontsize)
-plt.ylabel('CDF', fontsize=fontsize)
-# ax.set_xscale('log', base=2)
-# x_ticks = np.arange(0, 300*1024, 64*1024)
-y_ticks = np.arange(0, 1.2, 0.2)
-# plt.xticks(x_ticks)
-plt.yticks(y_ticks)
-plt.legend(loc='best', ncol=3, fontsize=fontsize)
-print('size', size_x)
-
-pngfile = 'imgs/dataset/twitter-cdf.png'
+pngfile = 'imgs/dataset/twitter-scope.png'
 plt.savefig(pngfile, dpi=600, bbox_inches='tight')
