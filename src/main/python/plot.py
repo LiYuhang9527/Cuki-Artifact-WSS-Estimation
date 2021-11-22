675874b0b06b6a26c76ee50ce92d069dec1bb55b
iluoeli <luoyili233@gmail.com>
2021-11-22 09:50:32 +0800

    Add plot clock memory ws

--- src/main/python/plot.py
+++ src/main/python/plot.py
@@ -11,7 +11,7 @@ def load_csv(filename):
 
 # 0             1       2           3   4           5       6
 # #operation	Real	Real(bytes)	MBF	MBF(bytes)	CCF	CCF(bytes)
-filename = '../../../benchmarks/benchmark/test.csv'
+filename = '../benchmarks/benchmark/test.csv'
 
 # parse arguments
 if len(sys.argv) > 1:
@@ -28,26 +28,27 @@ print('Average Error of CCF(Bytes)', np.divide(X[:, 6], X[:, 2]).mean() - 1.0)
 
 fontsize = 12
 num_subplots = 2
-plt.figure(0, figsize=(16, 2 * 4))
+plt.figure(0, figsize=(12, num_subplots * 4))
 
 # 1. Number
 plt.subplot(num_subplots, 1, 1)
-plt.plot(X[:, 0], X[:, 1], lw=1, ls='-', color='k', label='Real(Number)')
+plt.plot(X[:, 0], X[:, 1], lw=1, ls='-', color='k', label='Exact(Number)')
 plt.plot(X[:, 0], X[:, 3], lw=1, ls='--', color='c', label='MBF(Number)')
 plt.plot(X[:, 0], X[:, 5], lw=1, ls=':', color='r', label='CCF(Number)')
 plt.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
-plt.legend(loc='upper left', ncol=3, fontsize=fontsize)
-plt.ylabel('WSS (Number)', fontsize=fontsize)
+plt.legend(loc='best', ncol=3, fontsize=fontsize)
+plt.ylabel('Working Set Size (Number)', fontsize=fontsize)
+plt.xticks([])
 
 # 2. Bytes
 plt.subplot(num_subplots, 1, 2)
-plt.plot(X[:, 0], X[:, 2], lw=1, ls='-', color='k', label='Real(Bytes)')
+plt.plot(X[:, 0], X[:, 2], lw=1, ls='-', color='k', label='Exact(Bytes)')
 plt.plot(X[:, 0], X[:, 4], lw=1, ls='--', color='c', label='MBF(Bytes)')
 plt.plot(X[:, 0], X[:, 6], lw=1, ls=':', color='r', label='CCF(Bytes)')
 plt.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
-plt.legend(loc='upper left', ncol=3, fontsize=fontsize)
-plt.ylabel('WSS (Bytes)', fontsize=fontsize)
+plt.legend(loc='best', ncol=3, fontsize=fontsize)
+plt.ylabel('Working Set Size (Bytes)', fontsize=fontsize)
 
-plt.xlabel('#operation', fontsize=fontsize)
+plt.xlabel('Time', fontsize=fontsize)
 
 plt.savefig(filename.replace("csv", "png"), dpi=600, bbox_inches='tight')
