998b888f858cc49a41fae0234583fd9cf6ba0af0
iluoeli <luoyili233@gmail.com>
2021-12-01 13:43:49 +0800

    Improve ThroughputBenchmark

--- src/main/java/alluxio/client/file/cache/benchmark/ThroughputBenchmark.java
+++ src/main/java/alluxio/client/file/cache/benchmark/ThroughputBenchmark.java
@@ -13,14 +13,13 @@ package alluxio.client.file.cache.benchmark;
 
 import alluxio.client.file.cache.PageId;
 import alluxio.client.file.cache.ShadowCache;
+import alluxio.client.file.cache.dataset.ClientDataset;
 import alluxio.client.file.cache.dataset.Dataset;
 import alluxio.client.file.cache.dataset.DatasetEntry;
-import alluxio.client.file.cache.dataset.GeneralDataset;
-import alluxio.client.file.cache.dataset.generator.RandomEntryGenerator;
+import alluxio.client.file.cache.dataset.generator.EntryGenerator;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class ThroughputBenchmark implements Benchmark {
   private final BenchmarkContext mBenchmarkContext;
@@ -28,26 +27,44 @@ public class ThroughputBenchmark implements Benchmark {
   private final int mNumThreads;
   private final ShadowCache mShadowCache;
   private final List<CacheClient> mClients = new LinkedList<>();
-  private final List<Thread> mThreads = new LinkedList<>();
+  private EntryGenerator<String> mEntryGenerator;
+  private long mNumPeriodToRun;
 
   public ThroughputBenchmark(BenchmarkContext benchmarkContext, BenchmarkParameters parameters) {
     mBenchmarkContext = benchmarkContext;
     mBenchmarkParameters = parameters;
     mNumThreads = parameters.mNumThreads;
+    mEntryGenerator = BenchmarkUtils.createGenerator(parameters);
     mShadowCache = ShadowCache.create(parameters);
     mShadowCache.stopUpdate();
   }
 
   @Override
   public boolean prepare() {
+    // dispatch entries to each client's dataset
+    long startTick = System.currentTimeMillis();
+    long count = 0;
+    LinkedList<ClientDataset<String>> clientDatasets = new LinkedList<>();
     for (int i = 0; i < mNumThreads; i++) {
-      GeneralDataset<String> dataset = new GeneralDataset<>(
-          new RandomEntryGenerator(mBenchmarkParameters.mMaxEntries, 1,
-              (int) mBenchmarkParameters.mNumUniqueEntries + 1, 1, 1024, 1, 1001 + i),
-          (int) mBenchmarkParameters.mWindowSize);
-      mClients.add(new CacheClient(i, mShadowCache, dataset));
-      mThreads.add(new Thread(mClients.get(i)));
+      clientDatasets.add(new ClientDataset<>());
     }
+    int nextThread = 0;
+    while (mEntryGenerator.hasNext() && count < mBenchmarkParameters.mMaxEntries) {
+      clientDatasets.get(nextThread).insertEntry(mEntryGenerator.next());
+      nextThread = (nextThread + 1) % mNumThreads;
+      count++;
+    }
+    long periodSize = mBenchmarkParameters.mWindowSize / mBenchmarkParameters.mAgeLevels;
+    mNumPeriodToRun = count / periodSize;
+    long clientWindowSize =
+        mBenchmarkParameters.mWindowSize / mNumThreads / mBenchmarkParameters.mAgeLevels;
+    for (int i = 0; i < mNumThreads; i++) {
+      mClients.add(new CacheClient(i, mShadowCache, clientDatasets.get(i), clientWindowSize));
+    }
+    long duration = (System.currentTimeMillis() - startTick);
+    System.out.printf("Prepare %d entries cost %d ms\n", count, duration);
+    System.out.printf("numPeriodToRun %d, clientPeriodSize %d\n", mNumPeriodToRun,
+        clientWindowSize);
     return true;
   }
 
@@ -58,46 +75,69 @@ public class ThroughputBenchmark implements Benchmark {
     System.out.println(mShadowCache.getSummary());
     System.out.printf("num_threads=%d\n", mNumThreads);
     long startTick = System.currentTimeMillis();
-    for (int i = 0; i < mNumThreads; i++) {
-      mThreads.get(i).start();
-    }
-    long totalOperations = 0;
-    for (int i = 0; i < mNumThreads; i++) {
-      try {
-        mThreads.get(i).join();
-      } catch (Exception e) {
-        e.printStackTrace();
+    for (long w = 0; w < mNumPeriodToRun; w++) {
+      List<Thread> threads = new LinkedList<>();
+      for (int i = 0; i < mNumThreads; i++) {
+        threads.add(new Thread(mClients.get(i)));
+        threads.get(i).start();
       }
-      totalOperations += mClients.get(i).mOperationCount.get();
+      for (int i = 0; i < mNumThreads; i++) {
+        try {
+          threads.get(i).join();
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+      mShadowCache.aging();
     }
+    long opsdone = 0;
+    long runtime = 0;
+    for (int i = 0; i < mNumThreads; i++) {
+      opsdone += mClients.get(i).mOpsDone;
+      runtime += mClients.get(i).mRuntime;
+    }
+    long runtimePerClient = runtime / mNumThreads;
     long duration = (System.currentTimeMillis() - startTick);
-    System.out.printf("Cost %d ms, Throughput %d ops/sec\n", duration,
-        totalOperations * 1000 / duration);
+    System.out.printf("Insert %d entries, cost %d ms, Throughput %d ops/sec\n", opsdone, duration,
+        opsdone * 1000 / runtimePerClient);
   }
 
   private static class CacheClient implements Runnable {
     private final int mThreadId;
     private final ShadowCache mShadowCache;
     private final Dataset<String> mClientDataset;
-    private final AtomicLong mOperationCount = new AtomicLong(0);
+    private final long mWindowSize;
+    private long mOpsDone = 0;
+    private long mRuntime = 0;
 
-    public CacheClient(int threadId, ShadowCache shadowCache, Dataset<String> clientDataset) {
+    public CacheClient(int threadId, ShadowCache shadowCache, Dataset<String> clientDataset,
+        long windowSize) {
       mThreadId = threadId;
       mShadowCache = shadowCache;
       mClientDataset = clientDataset;
+      mWindowSize = windowSize;
     }
 
     @Override
     public void run() {
-      while (mClientDataset.hasNext()) {
+      runOneWindow();
+    }
+
+    private void runOneWindow() {
+      long startTick = System.currentTimeMillis();
+      int count = 0;
+      while (mClientDataset.hasNext() && count < mWindowSize) {
+        count++;
         DatasetEntry<String> entry = mClientDataset.next();
-        mOperationCount.incrementAndGet();
         PageId item = new PageId(entry.getScopeInfo().toString(), entry.getItem().hashCode());
         int nread = mShadowCache.get(item, entry.getSize(), entry.getScopeInfo());
         if (nread <= 0) {
           mShadowCache.put(item, entry.getSize(), entry.getScopeInfo());
         }
+        mShadowCache.updateTimestamp(1);
       }
+      mOpsDone += count;
+      mRuntime += (System.currentTimeMillis() - startTick);
     }
   }
 }
